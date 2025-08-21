79b62593d19224e35fadfa400422b7dc40fe761c
Achyutkulkarni-rgb <achyutkulkarni105@gmail.com>
2025-08-21 16:09:29 +0530

    Fix authMiddleware import path case sensitivity

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const { protect } = require("../middleware/authmiddleware");
+const { protect } = require("../Middleware/authmiddleware");
 
 const router = express.Router();
 
